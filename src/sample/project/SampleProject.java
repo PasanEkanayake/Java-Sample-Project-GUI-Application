d3003d8ab48f4948ddc8162dfc4b132ffa350286
Pasan Ekanayake <pasanekanayake@gmail.com>
2024-09-27 00:12:42 +0530

    commit

--- src/sample/project/SampleProject.java
+++ src/sample/project/SampleProject.java
@@ -1,9 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 package sample.project;
 
+/**
+ *
+ * @author Pasan Ekanayake
+ */
 public class SampleProject {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        
+        try{
+            
+            Login login = new Login();
+            login.setVisible(true);
+        }
+        catch (Exception ex){
+            
+        }
     }
     
 }
