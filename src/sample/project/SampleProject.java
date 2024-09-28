c84cf0f9176b01681b88c0e2d6fc39f0fd8e39d1
Pasan Ekanayake <pasanekanayake@gmail.com>
2024-09-28 05:47:45 +0530

    commit

--- src/sample/project/SampleProject.java
+++ src/sample/project/SampleProject.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package sample.project;
 
-/**
- *
- * @author Pasan Ekanayake
- */
 public class SampleProject {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         try{
             
@@ -25,3 +14,4 @@ public class SampleProject {
     }
     
 }
+//Create method in the main file to connect the database and call them in other files
