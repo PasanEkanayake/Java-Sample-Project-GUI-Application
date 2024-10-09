34190f50e8ee9f581333673c1a63b9cf0e12c25c
Pasan Ekanayake <pasanekanayake@gmail.com>
2024-10-09 12:58:48 +0530

    Update Login.java

--- src/sample/project/Login.java
+++ src/sample/project/Login.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package sample.project;
 
 import java.sql.*;
@@ -11,10 +7,6 @@ import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
 import javax.swing.RowFilter;
 
-/**
- *
- * @author Pasan Ekanayake
- */
 public class Login extends javax.swing.JFrame {
 
     private static final String username = "root";
